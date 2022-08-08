032fa74312a3b41d3da16ed208ff31fb36d5dcc8
Raza Khadim <razakhadim@yahoo.com>
2022-08-09 09:40:56 +1200

    tailwindcss setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  content: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    fontFamily: {
-      'sans': 'Roboto, Arial, sans-serif',
-      'serif': 'Merriweather, Georgia, serif'
-    },
-    extend: {
-      colors: {
-        'primary': '#672E9B',
-        gray: {
-          300: '#f0f0f0',
-        }
-      }
-    },
+    extend: {},
   },
-  variants: {},
   plugins: [],
 }
